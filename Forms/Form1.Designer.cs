f616c4b09ba4384c962d56a3ceb36bb9e2841dd5
Arttu Höylä <aahoo01@gmail.com>
2018-08-29 12:00:04 +0300

    Completed T2

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -45,15 +45,15 @@
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
             this.infoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.Arvio = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -81,67 +81,72 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(115, 107);
+            this.textBox4.Location = new System.Drawing.Point(115, 179);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(144, 20);
             this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "0";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 107);
+            this.label6.Location = new System.Drawing.Point(22, 179);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 6;
-            this.label6.Text = "label6";
+            this.label6.Text = "Kesto (Min):";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(115, 81);
+            this.textBox3.Location = new System.Drawing.Point(115, 113);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
             this.textBox3.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 81);
+            this.label5.Location = new System.Drawing.Point(22, 113);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(73, 13);
             this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
+            this.label5.Text = "Julkaisuvuosi:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(115, 55);
+            this.textBox2.Location = new System.Drawing.Point(115, 66);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "[Syötä elokuvan nimi]";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 55);
+            this.label4.Location = new System.Drawing.Point(22, 66);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(30, 13);
             this.label4.TabIndex = 2;
-            this.label4.Text = "label4";
+            this.label4.Text = "Nimi:";
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(115, 29);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(22, 29);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(21, 13);
             this.label3.TabIndex = 0;
-            this.label3.Text = "label3";
+            this.label3.Text = "ID:";
             // 
             // menuStrip1
             // 
@@ -194,6 +199,7 @@
             this.infoToolStripMenuItem1.Name = "infoToolStripMenuItem1";
             this.infoToolStripMenuItem1.Size = new System.Drawing.Size(206, 22);
             this.infoToolStripMenuItem1.Text = "Info";
+            this.infoToolStripMenuItem1.Click += new System.EventHandler(this.infoToolStripMenuItem1_Click);
             // 
             // groupBox2
             // 
@@ -203,9 +209,39 @@
             this.groupBox2.Location = new System.Drawing.Point(337, 27);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(449, 701);
-            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Toiminallisuus";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(87, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 92);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Poista";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(87, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Tallenna";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Uusi Tietue";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Arvio
             // 
@@ -217,7 +253,7 @@
             this.Arvio.Location = new System.Drawing.Point(12, 349);
             this.Arvio.Name = "Arvio";
             this.Arvio.Size = new System.Drawing.Size(319, 379);
-            this.Arvio.TabIndex = 3;
+            this.Arvio.TabIndex = 2;
             this.Arvio.TabStop = false;
             this.Arvio.Text = "Arvio";
             // 
@@ -259,37 +295,7 @@
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(307, 276);
             this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(87, 41);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(280, 92);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Uusi Tietue";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Lime;
-            this.button2.Location = new System.Drawing.Point(87, 139);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(280, 92);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Tallenna";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.ForeColor = System.Drawing.Color.Black;
-            this.button3.Location = new System.Drawing.Point(87, 237);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(280, 92);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Poista";
-            this.button3.UseVisualStyleBackColor = false;
+            this.richTextBox1.Text = "[Kirjoita Arvostelu Tähän]";
             // 
             // Form1
             // 
--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -15,6 +15,65 @@ namespace Forms
         public Form1()
         {
             InitializeComponent();
+            textBox2.Enter += new EventHandler(textBox2_Enter);
+            textBox2.Leave += new EventHandler(textBox2_Leave);
+            textBox3.Enter += new EventHandler(textBox3_Enter);
+            textBox3.Leave += new EventHandler(textBox3_Leave);
+            textBox4.Enter += new EventHandler(textBox4_Enter);
+            textBox4.Leave += new EventHandler(textBox4_Leave);
+            richTextBox1.Enter += new EventHandler(richTextBox1_Enter);
+            richTextBox1.Leave += new EventHandler(richTextBox1_Leave);
+            textBox3.Text = DateTime.Now.Year.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void infoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ohjelman teki Arttu Höylä","Info");
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if(textBox2.Text == "") textBox2.Text = "[Syötä elokuvan nimi]";
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "") textBox3.Text = DateTime.Now.Year.ToString();
+        }
+
+        private void textBox4_Enter(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "") textBox4.Text = "0";
+        }
+
+        private void richTextBox1_Enter(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void richTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "") richTextBox1.Text = "[Kirjoita Arvostelu Tähän]";
         }
     }
 }
